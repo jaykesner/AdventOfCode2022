6d26b0c9c3107c22034511551995699774616f02
Jay Kesner <jaykesner@gmail.com>
2022-11-23 16:53:10 -0500

    mess around

--- hello.js
+++ hello.js
@@ -39,4 +39,55 @@ const toWeirdCase = (strings) =>
     )
     .join(" ");
 
-console.log(toWeirdCase("This is words"));
+//console.log(toWeirdCase("This is words"));
+
+/*
+You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+
+Find two lines that together with the x-axis form a container, such that the container contains the most water.
+
+Return the maximum amount of water a container can store.
+
+Input: height = [1,8,6,2,5,4,8,3,7]
+Output: 49
+Explanation: The above vertical lines are represented by array [1,8,6,2,5,4,8,3,7]. In this case, the max area of water (blue section) the container can contain is 49.
+Example 2:
+
+Input: height = [1,1]
+Output: 1
+*/
+
+const calcArea = (height, minPos, maxPos) => {
+  return Math.min(height[minPos], height[maxPos]) * (maxPos - minPos);
+};
+
+//let height = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+let height = [2, 3, 4, 5, 18, 17, 6];
+const maxArea2 = height.reduce((maxArea, value, index) => {
+  let newArea = calcArea(height, index, height.length - 1);
+  if (maxArea < newArea) {
+    return (maxArea = newArea);
+  } else {
+    return maxArea;
+  }
+}, 0);
+
+console.log(maxArea2);
+
+var maxArea = function (height) {
+  let minPos = 0;
+  let maxPos = height.length - 1;
+  let maximum = calcArea(height, minPos, maxPos);
+  while (minPos != maxPos) {
+    if (height[maxPos] > height[minPos]) {
+      minPos += 1;
+    } else {
+      maxPos -= 1;
+    }
+    const newArea = calcArea(height, minPos, maxPos);
+    if (maximum < newArea) maximum = newArea;
+  }
+  return maximum;
+};
+
+//console.log(maxArea(test));

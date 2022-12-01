bb38abf24527092e835ec40f125902f127a60b9b
Jay Kesner <jaykesner@gmail.com>
2022-12-01 10:47:22 -0500

    day 1

--- day1/day1.js
+++ day1/day1.js
@@ -1,3 +1,53 @@
-const { day1input } = require("./day1input");
+const input = require("./day1input");
 
-console.log(day1input);
+const testInput = `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`;
+
+const splitInventories = input.split("\n");
+let largestInventory = 0;
+let currentInventorysize = 0;
+const totalInventories = [];
+
+function checkInventory(reset) {
+  if (currentInventorysize > largestInventory) {
+    largestInventory = currentInventorysize;
+  }
+  if (reset) {
+    totalInventories.push(currentInventorysize);
+    currentInventorysize = 0;
+  }
+}
+
+for (let i = 0; i < splitInventories.length; i++) {
+  if (splitInventories[i] == "") {
+    checkInventory(true);
+  } else {
+    if (i == splitInventories.length - 1) {
+      currentInventorysize += parseInt(splitInventories[i]);
+      checkInventory(true);
+    } else {
+      currentInventorysize += parseInt(splitInventories[i]);
+      checkInventory(false);
+    }
+  }
+}
+
+const topThreeInventories = totalInventories
+  .sort((a, b) => a - b)
+  .splice(totalInventories.length - 3)
+  .reduce((top, total) => (top += total));
+
+console.log(largestInventory);
+console.log(topThreeInventories);
